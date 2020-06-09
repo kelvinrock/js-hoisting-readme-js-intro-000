09884721008808608408bb29bdc0b319de05ba18
Kelvin Jayaram <kelvin.rock.aplus@gmail.com>
2020-06-09 17:29:19 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,7 +10,7 @@ function callMe() {
 
 
 function crazy() {
-  var crazy;// fix the code in here:
+  console.log("hey!!!")// fix the code in here:
   thisIsCrazy();
 
   var thisIsCrazy = function (){
